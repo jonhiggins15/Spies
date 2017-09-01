abe5236b11164c75ebd7c6157302941c75edf317
jonhiggins15 <jonathanhiggins15@gmail.com>
2017-09-01 17:12:37 -0400

    Better mobile look and started on rules
    Feel free to change the way the word "Spies" looks. Playing around with it

--- HTML/index.js
+++ HTML/index.js
@@ -62,34 +62,3 @@ $('#signOut').click(function(){
 $('#signOut').click(function(){
   $('#loginModal').modal('show');
 });
-
-
-
-/*
-var counter = 0;
-function spy(value){
-  $(document).ready(function(){
-    var spy={
-      name: "Spy",
-      life: true,
-      killer: true,
-      icon: new Image()
-    };
-    counter++;
-    spy.icon.src = "Images/waves.jpg";
-    $('#test').html(spy.icon);
-    $("#thing").html(counter);
-
-        if(counter<3 || counter>7){
-          $("#testing").html("True");
-          $("#test").fadeTo("slow", 1);
-          spy.life = true;
-        }
-        else{
-          //$("#testing").html("The " + spy.name + " is dead");
-          $("#test").fadeTo("slow", .5);
-          spy.life = false;
-        }
-      });
-}
-*/
